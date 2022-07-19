717bdbe395f50a07670f56f8847cfdfb8354bfd4
GrazewalSingh <singhgrazewal2@gmail.com>
2022-07-19 16:34:57 +0530

    Header done day-1

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Container,HStack,Link,Image,Select,option,Text,Button, Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,} from '@chakra-ui/react'
+ import {TriangleDownIcon,LinkIcon} from "@chakra-ui/icons"
+import Search from './Search'
+ 
 const Header = () => {
+ 
   return (
-    <div>Header</div>
+    <Container padding={5} maxW='container.xl' >
+        <HStack spacing="auto">
+            <HStack spacing={8} width="60%">
+              <Image marginTop={-5} width="70px" src="https://ketto.gumlet.io/assets/images/logo-light-bg.svg?w=70&dpr=1.0"/>
+              <Link _hover={{textDecoration:"none"}}>Browser Fundraiser</Link>
+              <Menu>
+                <MenuButton padding={2} fontWeight={400} variant="unstyled"  as={Button} rightIcon={<TriangleDownIcon fontSize={10}/>}>
+                Fundraiser For
+                </MenuButton>
+                <MenuList >
+                  <MenuItem  _hover={{ color: '#30C9C8' }} >Medical Treatment</MenuItem>
+                  <MenuItem  _hover={{ color: '#30C9C8' }}  >NGO / Charity</MenuItem>
+                  <MenuItem _hover={{ color: '#30C9C8' }}  >Other Cause</MenuItem>
+                </MenuList>
+            </Menu>
+              <Link _hover={{textDecoration:"none"}}>How It Works</Link>
+               <Text> | </Text>
+              <Search/>
+            </HStack>
+            <HStack spacing="5">
+            <Button height={8} color="green" border="2px solid green" fontWeight={500}  background="none" _hover={{background:"none"}} leftIcon={<LinkIcon/>} >Chat</Button>
+              <Button height={8} color="#30C9C8" border="2px solid #30C9C8}" fontWeight={500}  background="none" _hover={{background:"#EAF9F9"}}>Start a Fundraiser</Button>
+              <Button height={8}  fontWeight={400} variant="ghost"  _hover={{background:"none"}} >Sign In</Button>
+            </HStack>
+        </HStack>
+    </Container>
   )
 }
 
---
+++ src/Components/Search.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react'
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,Button,Input,Container,HStack,Icon,Box,Link
+  } from '@chakra-ui/react'
+  import {Search2Icon,DeleteIcon} from "@chakra-ui/icons"
+import { useDisclosure } from '@chakra-ui/react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+const Search = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [value,setvalue]=useState("")
+    const [suggestion,setSuggestion]=useState([])
+    const data=useSelector((state)=>state.AppReducer.funds)
+    console.log(data)
+    useEffect(()=>{
+        let newlistofsuggestions = data.filter((item)=> item.title.toLowerCase().indexOf(value)!==-1?true:false).map((item)=>item);
+        setSuggestion(newlistofsuggestions)
+    },[value])
+  return (
+    <>
+      <Button fontWeight={400} leftIcon={<Search2Icon/>} variant="unstyled" onClick={onOpen}>
+       Search
+      </Button>
+
+      <Drawer placement="top" onClose={onClose} isOpen={isOpen}>
+       <DrawerOverlay />
+        <DrawerContent >
+        <Container maxW='container.xl'  borderBottom=".5px solid grey">
+          <DrawerHeader border="none" borderBottomWidth='1px' fontSize={16} color="#30C9C8">SEARCH KETTO</DrawerHeader>
+          <DrawerBody>
+            <HStack>
+                <Icon color="#30C9C8" as={Search2Icon} fontSize={20} marginRight={2}/>
+                <Input value={value} fontSize={24} size="lg" variant='unstyled' placeholder='Search by fundraisers, fundraiser creator, beneficiary or NGOs'onChange={(e)=>{setvalue(e.target.value)}} />
+                {value&&<Icon cursor="pointer" color="#30C9C8" as={DeleteIcon} fontSize={20} onClick={()=>setvalue("")}/>}
+            </HStack >
+           
+          </DrawerBody>
+          </Container>
+          <Box margin="auto" >
+            <Container margin={5}>
+                    {suggestion.length===0&&value&& <p>Sorry, no results found</p>}
+            </Container>
+        </Box>
+          <Box backgroundColor="#F5F5F5" marginTop={10} minH="16">
+             <Container maxW="container.xl" marginTop={4}>
+                <Link color="#30C9C8">Click here</Link> to view fundraisers you have supported on Ketto
+             </Container>
+         </Box>
+        </DrawerContent>
+        
+      </Drawer>
+      </>
+  )
+}
+
+export default Search
