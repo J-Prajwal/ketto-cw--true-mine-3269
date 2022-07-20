64590e300e5703bb849dc1a40e333f3811d39b56
Sangam Kumar Singh <99814514+sangamk04@users.noreply.github.com>
2022-07-21 00:41:37 +0530

    day1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import MainRoutes from "./Pages/MainRoutes";
+import logo from './logo.svg';
+import './App.css';
+import HowWorks from './Components/HowWorks';
+import Carousel from './Components/Carousal';
+import Faq from './Components/Faq';
 
 function App() {
   return (
-    <div>
-      <MainRoutes />
+    <div className="App">
+     <HowWorks/>
+     {/* <Faq/> */}
+   
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./Redux/Store";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </ChakraProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
