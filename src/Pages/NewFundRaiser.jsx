55e8153d4452c0a0d1d1dd1eaff5eb2166c8e0d7
Masrur Alam <masrur.jsr@gmail.com>
2022-07-20 22:57:22 +0530

    Day-2 commit

--- src/Pages/NewFundRaiser.jsx
+++ src/Pages/NewFundRaiser.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { IconButton, Input, Stack, MdPhone, Box } from "@chakra-ui/react";
+import { BiEnvelope } from "react-icons/bi";
+import React from "react";
 
 const NewFundRaiser = () => {
   return (
-    <div>NewFundRaiser</div>
-  )
-}
+    <div>
+      <Stack spacing={3} alignItems="center">
+        <Input variant="flushed" placeholder="Name" width={"30%"} />
+        <Input variant="flushed" placeholder="Emil Address" width={"30%"} />
+        <Input
+          variant="flushed"
+          placeholder="Create a Password"
+          width={"30%"}
+        />
 
-export default NewFundRaiser
+        <Input variant="flushed" placeholder="Mobile" width="30%" />
+        <BiEnvelope />
+      </Stack>
+    </div>
+  );
+};
+
+export default NewFundRaiser;
