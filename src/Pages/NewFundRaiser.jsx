edc472b8b843062ffa50677e116133ad4d0c35e3
Masrur Alam <masrur.jsr@gmail.com>
2022-07-21 15:06:43 +0530

    let's some progress in Day1

--- src/Pages/NewFundRaiser.jsx
+++ src/Pages/NewFundRaiser.jsx
@@ -1,10 +1,71 @@
-import { IconButton, Input, Stack, MdPhone, Box, Text } from "@chakra-ui/react";
-// import { BiEnvelope } from "react-icons/bi";
-import React from "react";
+import {
+  IconButton,
+  Input,
+  Stack,
+  MdPhone,
+  Box,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  Button,
+  MenuItem,
+  Flex,
+  Select,
+} from "@chakra-ui/react";
+import { ChevronDownIcon, PhoneIcon, ViewOffIcon } from "@chakra-ui/icons";
+
+import { BiEnvelope } from "react-icons/bi";
+import React, { useState } from "react";
 
 const NewFundRaiser = () => {
+  const [menu, setMenu] = useState("English");
   return (
-    <Box bg="#3d3d3d" w={"100%"} h={"641px"}>
+    <Box bg="#3d3d3d" w={"100%"} h={"741px"}>
+      <br />
+      {/* <Select onChange={(e) => setMenu(e.target.value)}>
+        <option value="English">English</option>
+        <option value="Hindi">Hindi</option>
+        <option value="English">English</option>
+        <option value="English">English</option>
+      </Select> */}
+      <Menu bg="white" marginLeft="530px">
+        <MenuButton
+          outline={"none"}
+          border={""}
+          bg={"white"}
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          // rightIcon={<BiEnvelope />}
+        >
+          {menu}
+        </MenuButton>
+        <MenuList
+          onChange={(e) => {
+            setMenu(e.target.value);
+          }}
+          mt={"-10"}
+          borderRadius={"15px"}
+          width={"200px"}
+        >
+          <MenuItem value={"English"} borderRadius={"15px 15px 0px 0px"}>
+            English
+          </MenuItem>
+          <MenuItem
+            onClick={(e) => {
+              setMenu(e.target.value);
+            }}
+            value={"Hindi"}
+          >
+            Hindi
+          </MenuItem>
+          <MenuItem value={"Tamil"}>Tamil</MenuItem>
+          <MenuItem value={"Bengali"} borderRadius={"0px 0px 15px 15px"}>
+            Bengali
+          </MenuItem>
+        </MenuList>
+      </Menu>
+      <br />
       <Box
         w="500px"
         p="15px"
@@ -12,36 +73,78 @@ const NewFundRaiser = () => {
         border={"2px solid green"}
         m={"0px 20px 0px 530px"}
         bg="white"
-        borderRadius="15px"
+        borderRadius="10px 10px 0px 0px"
       >
         <Text
           fontSize="16px"
           color="black"
           fontWeight="bold"
           fontFamily="Helvetica"
+          textAlign={"center"}
         >
           Start your fund Raiser
         </Text>{" "}
         <hr />
         <br />
-        <Box borderRadius="25px" backgroundColor="grey" padding={"10px"}>
+        <Box
+          m="auto"
+          borderRadius="25px"
+          backgroundColor="whitesmoke"
+          padding={"0px"}
+          display="flex"
+          w="90%"
+          justifyContent={"space-around"}
+        >
           {" "}
-          <Text>Purpose of raising funds </Text>{" "}
+          <Text ml={"35px"} lineHeight={"45px"}>
+            Purpose of raising funds{" "}
+          </Text>{" "}
+          <Menu ml="80px">
+            <MenuButton
+              outline={"none"}
+              border={"none"}
+              bg={"transparent"}
+              as={Button}
+              rightIcon={<ChevronDownIcon />}
+              // rightIcon={<BiEnvelope />}
+            >
+              Medical Treatment
+            </MenuButton>
+            <MenuList mt={"-10"} borderRadius={"15px"} width={"200px"}>
+              <MenuItem borderRadius={"15px 15px 0px 0px"}>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem borderRadius={"0px 0px 15px 15px"}>Delete</MenuItem>
+            </MenuList>
+          </Menu>
         </Box>
         <br />
         <Stack spacing={3} alignItems="center">
-          <Input variant="flushed" placeholder="Name" />
+          <Flex w="100%">
+            <Input variant="flushed" placeholder="Name" />
+            <BiEnvelope />
+          </Flex>
+
           <br />
-          <Input variant="flushed" placeholder="Emil Address" />
+          <Flex w="100%">
+            <Input variant="flushed" placeholder="Emil Address" />
+            <BiEnvelope />
+          </Flex>
           <br />
-          <Input variant="flushed" placeholder="Create a Password" />
+          <Flex w="100%">
+            <Input variant="flushed" placeholder="Create a Password" />
+            <ViewOffIcon />
+          </Flex>
+
           <br />
-          <Input variant="flushed" placeholder="Mobile" />
+          <Flex w="100%">
+            <Input variant="flushed" placeholder="Mobile" />
+            <PhoneIcon />
+          </Flex>
           <br />
           <br />
           <br />
-          <br />
-          <br />
+
           <br />
           <br />
           <box display="flex">
@@ -63,6 +166,16 @@ const NewFundRaiser = () => {
             </Text>
           </box>
         </Stack>
+      </Box>
+      <Box
+        ml={"531px"}
+        bgColor={"rgb(1,191,189)"}
+        borderRadius={"0px 0px 10px 10px"}
+        h="55px"
+        w="499px"
+        fontFamily={"sans-serif"}
+      >
+        Next{" "}
       </Box>
     </Box>
   );
