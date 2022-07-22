e17c86cf65dc5ae52d9a540d0e2c8270e65cb7c8
Masrur Alam <masrur.jsr@gmail.com>
2022-07-22 20:03:24 +0530

    NewFund Raiser 3

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -6,6 +6,7 @@ import Homepage from "./Homepage";
 import HowItWorks from "./HowItWorks";
 import NewFundRaiser from "./NewFundRaiser";
 import NewFundRaiser2 from "./NewFundRaiser2";
+import NewFundRaiser3 from "./NewFundRaiser3";
 
 const MainRoutes = () => {
   return (
@@ -19,7 +20,7 @@ const MainRoutes = () => {
         path="/new/crowdfunding/type=:cat"
         element={
           // <RequiredAuth>
-          <NewFundRaiser2 />
+          <NewFundRaiser3 />
           // </RequiredAuth>
         }
       ></Route>
