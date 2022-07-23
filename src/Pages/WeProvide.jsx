6844e2d7c296e8f5f828b471fa523dc2e73e4b51
sangamk04 <99814514+sangamk04@users.noreply.github.com>
2022-07-23 11:16:26 +0530

    Correct  some  miss types

--- src/Pages/WeProvide.jsx
+++ src/Pages/WeProvide.jsx
@@ -264,10 +264,10 @@ const WeProvide = () => {
     <HStack style={{ marginLeft:"20rem", padding:"20px ", marginTop:"3rem"
     }}>
   <Button backgroundColor={"#30C9C8"} color={"white"} padding={"25"} >
-   SATRT A FUNDRAISER
+   START A FUNDRAISER
   </Button>
   <Button color={"#30C9C8"} backgroundColor={"white"} padding={"25"} border={"1px solid #30C9C8 "}>
-    BROUSE FUNDRAISER
+    BROWSW FUNDRAISER
   </Button>
 </HStack>
 
